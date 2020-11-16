8072fecf70c1ec67423d2f3b67299c02ab0bca64
Kewei Zhang <kewei.zhang@kasna.com.au>
2020-11-17 10:15:58 +1100

    update bootstrap cb module

--- modules/bootstrap-cloudbuild/module/main.tf
+++ modules/bootstrap-cloudbuild/module/main.tf
@@ -2,22 +2,16 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-locals {
-  env               = lookup(var.substitution_vars, "_ENV", "nonprod")
-  plan_branch_name  = lookup(var.substitution_vars, "_ENV", "nonprod") == "prod" ? "^develop$" : "feature/*"
-  apply_branch_name = lookup(var.substitution_vars, "_ENV", "nonprod") == "prod" ? "^master$" : "develop"
-}
-
 # Create Cloud Source Repositories
-resource google_sourcerepo_repository terraform_gcp_foundation {
-  project = data.google_project.build_project.project_id
-  name    = "terraform-gcp-foundation"
-}
+# resource google_sourcerepo_repository terraform_gcp_foundation {
+#   project = data.google_project.build_project.project_id
+#   name    = "terraform-gcp-foundation"
+# }
 
-resource google_sourcerepo_repository terraform_gcp_module {
-  project = data.google_project.build_project.project_id
-  name    = "terraform-gcp-module"
-}
+# resource google_sourcerepo_repository terraform_gcp_module {
+#   project = data.google_project.build_project.project_id
+#   name    = "terraform-gcp-module"
+# }
 
 # Create Module CI Cloudbuild trigger for unit-testing based on feature branch
 # resource google_cloudbuild_trigger module_dry_run {
