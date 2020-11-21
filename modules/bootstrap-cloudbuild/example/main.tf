3e7afa555ddb1a53e075d500a2f3415de872d284
kewei5zhang <keweizhang411@gmail.com>
2020-11-21 22:46:19 +1100

    update testing data

--- modules/bootstrap-cloudbuild/example/main.tf
+++ modules/bootstrap-cloudbuild/example/main.tf
@@ -1,6 +1,6 @@
 module bootstrap-cloudbuild {
   source           = "../module/"
   build_project_id = "kewei-demo-sandbox"
-  env_names        = ["test_env"]
+  env_names        = ["test-env"]
   build_env        = "nonprod-build"
 }
