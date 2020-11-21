7a83cd4fea336247783e10dfc025bad2f91edf47
kewei5zhang <keweizhang411@gmail.com>
2020-11-21 22:57:49 +1100

    add cloud_builds topic data source

--- modules/bootstrap-cloudbuild/module/data_sources.tf
+++ modules/bootstrap-cloudbuild/module/data_sources.tf
@@ -1,3 +1,8 @@
 data google_project build_project {
   project_id = var.build_project_id
 }
+
+data google_pubsub_topic cloud_builds {
+  project = var.build_project_id
+  name    = "cloud-builds"
+}
